ba332028b15d7895a77f419bfeb5e504b4163673
javilujan2 <javilujan2@gmail.com>
2022-12-16 12:29:01 +0100

    Añadiendo altura triangulo

--- math.js
+++ math.js
@@ -36,6 +36,15 @@ function calcularTriangulo(lado1, lado2, base, altura) {
   };
 }
 
+function calcularAlturaTriangulo(lado1, base){
+  if (lado1 == base){
+    console.warn('Este no es un triangulo isosceles');
+  } else {
+    //h = raiz cuadrada (lado1**2 - (b**2) / 4)
+    return Math.sqrt( (lado1 **2) - ((base ** 2) / 4 ) )
+  }
+}
+
 console.log({
   ladoTriangulo1,
   ladoTriangulo2,
@@ -75,3 +84,4 @@ function CalcilarCirculo (radio) {
 }
 
 console.groupEnd('circle')
+
